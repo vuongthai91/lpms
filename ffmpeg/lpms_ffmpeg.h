e8cccfa2655239848c1b00dbcf2ac51e772e1f31
Josh Allmann <joshua.allmann@gmail.com>
2019-04-19 06:16:25 +0000

    ffmpeg: Initial configuration for GPU decoding.

--- ffmpeg/lpms_ffmpeg.h
+++ ffmpeg/lpms_ffmpeg.h
@@ -1,3 +1,4 @@
+#include <libavutil/hwcontext.h>
 #include <libavutil/rational.h>
 
 typedef struct {
@@ -8,8 +9,15 @@ typedef struct {
   AVRational fps;
 } output_params;
 
+typedef struct {
+  char *fname;
+
+  // Optional hardware acceleration
+  enum AVHWDeviceType hw_type;
+} input_params;
+
 void lpms_init();
 void lpms_deinit();
 int  lpms_rtmp2hls(char *listen, char *outf, char *ts_tmpl, char *seg_time, char *seg_start);
-int  lpms_transcode(char *inp, output_params *params, int nb_outputs);
+int  lpms_transcode(input_params *inp, output_params *params, int nb_outputs);
 int  lpms_length(char *inp, int ts_max, int packet_max);
